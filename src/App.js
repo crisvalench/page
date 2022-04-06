bcb447b822027280cc39931b486a03ea2e73ab9b
Cristina <cristinavalenzuela2002@hotmail.com>
2022-04-06 16:05:11 -0500

    Barra de navegacion

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+      <nav className='nav'>
+            <a href= "home" className='logo nav-link'>HOLIDAYZ</a> 
+        <ul className='nav-menu'>
+          <li className='nav-menu-item'>
+          <li><a href= "#home"className='nav-menu nav-link'>Home</a></li>          
+          <li><a href= "#events" className='nav-menu nav-link'>Events</a></li>
+          <li><a href= "#about" className='nav-menu nav-link'>About</a></li>
+          <li><a href= "#blog" className='nav-menu nav-link v'>Blog</a></li>
+          <li><a href= "#contact" className='nav-menu nav-link'> Contact</a></li>
+          <li><a href= "#contact" className='nav-menu nav-link number'> +1(123 4567890)</a></li>
+          </li>
+        </ul>
+        
+      </nav>
       </header>
+      <body>
+      </body>
     </div>
   );
 }
